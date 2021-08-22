fa083d6fe9b582361279562c91632a2326146a1b
william-zhang-ml <william.zhang.ml@gmail.com>
2021-08-22 11:19:24 -0400

    multiclass pred layer

--- wz_torch/__init__.py
+++ wz_torch/__init__.py
@@ -1,5 +1,6 @@
+from torch import Tensor
 import torch.nn as nn
-from torch.nn import Sequential
+from torch.nn import Module, Sequential
 
 
 class OneHiddenEmbedding(Sequential):
@@ -17,3 +18,38 @@ class OneHiddenEmbedding(Sequential):
         self.add_module('Norm0', nn.LayerNorm(embed_dim))
         self.add_module('Act0', nn.ReLU())
         self.add_module('Lin1', nn.Linear(embed_dim, embed_dim))
+
+
+class PredictOnAverage(Module):
+    """ Multiclass logistic regression on sequence average representation. """
+    def __init__(self, inp_dim: int, num_classes: int) -> None:
+        """ Constructor.
+
+        :param inp_dim:     input dimension
+        :type  inp_dim:     int
+        :param num_classes: number of target classes
+        :type  num_classes: int
+        """
+        super(PredictOnAverage, self).__init__()
+        self.pred_head = nn.Sequential(
+            nn.Linear(inp_dim, num_classes),
+            nn.Softmax(dim=-1))
+
+    def forward(self, seq: Tensor, mask: Tensor = None) -> Tensor:
+        """ Compute class confidence scores.
+
+        :param seq:  input sequences (T, B, D)
+        :type  seq:  Tensor
+        :param mask: marks where seq is not padded (T, B), defaults to None
+        :type  mask: Tensor, optional
+        :return:     class confidence scores (B, num_classes)
+        :rtype:      Tensor
+        """
+        if mask is None:
+            ave_repr = seq.mean(dim=0)            # B, D
+        else:
+            mask = mask.view(*mask.shape, 1)      # T, B, 1
+            seq_lens = mask.sum(dim=0)            # B, 1
+            sum_repr = (seq * mask).sum(dim=0)    # B, D
+            ave_repr = sum_repr / seq_lens        # B, D
+        return self.pred_head(ave_repr)           # B, num_classes
