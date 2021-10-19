22adf4d1f6b1015cde6c90a7847f6b12896b20c6
william-zhang-ml <william.zhang.ml@gmail.com>
2021-10-19 00:40:32 -0400

    function to visualize prior assignment

--- wz_torch/conv/yolo/__init__.py
+++ wz_torch/conv/yolo/__init__.py
@@ -1,4 +1,6 @@
 from typing import Iterable, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 from torch import Tensor
 import torch.nn as nn
@@ -150,3 +152,55 @@ def assign_to_prior(boxes: Iterable[Iterable[float]],
 
     iou = box_iou(boxes, priors)  # (N, P)
     return iou.argmax(dim=-1)     # match by IOU
+
+
+def plot_prior_regions(priors: Iterable[Iterable[float]],
+                       w_min: float,
+                       w_max: float,
+                       h_min: float,
+                       h_max: float,
+                       n_grid: int = 100,
+                       figsize: Tuple[float, float] = (8, 4.5)):
+    """ Make a plot showing prior boxes will claim bounding boxes.
+
+    :param priors:  prior boxes (width, height), (P, 2)
+    :type  priors:  Iterable[Iterable[float]]
+    :param w_min:   minimum bounding box width to consider
+    :type  w_min:   float
+    :param w_max:   maximum bounding box width to consider
+    :type  w_max:   float
+    :param h_min:   minimum bounding box height to consider
+    :type  h_min:   float
+    :param h_max:   maximum bounding box height to consider
+    :type  h_max:   float
+    :param n_grid:  num of width and height points considered, defaults to 100
+    :type  n_grid:  int, optional
+    :param figsize: figure width and height in inches
+    :type: figsize: Tuple[float, float]
+    :return:       figure and axes
+    :rtype:        -
+    """
+    priors = np.array(priors)  # cast
+
+    # define a grid of bounding boxes
+    wm, hm = np.meshgrid(
+        np.linspace(w_min, w_max, n_grid),
+        np.linspace(h_min, h_max, n_grid))
+    wm, hm = wm.reshape(n_grid ** 2), hm.reshape(n_grid ** 2)
+    boxes = np.stack([wm, hm], axis=-1)
+
+    # assign bounding boxes to priors
+    assign = assign_to_prior(boxes, priors)
+
+    # plot and annotate
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(priors[:, 0], priors[:, 1], 'wo', zorder=10)
+    for idx in range(len(priors)):
+        mask = assign == idx
+        ax.plot(boxes[mask, 0], boxes[mask, 1], '.', markersize=1)
+    ax.set_xlabel('width')
+    ax.set_ylabel('height')
+    ax.set_xlim([w_min, w_max])
+    ax.set_ylim([h_min, h_max])
+    fig.tight_layout()
+    return fig, ax
