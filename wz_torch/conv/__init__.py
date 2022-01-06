4532c9d30696eba2aa371e664d514af19bb6cc7d
william-zhang-ml <william.zhang.ml@gmail.com>
2022-01-06 14:48:39 -0500

    revised residual code, doc, org

--- wz_torch/conv/__init__.py
+++ wz_torch/conv/__init__.py
@@ -26,8 +26,10 @@ class ConvBatchRelu(Sequential):
         :type  kernel_size:  Union[int, Tuple[int, int]], optional
         :param stride:       stride height and width, defaults to 1
         :type  stride:       Union[int, Tuple[int, int]], optional
-        :param padding:      number of row and col pixels to pad, defaults to 1
+        :param padding:      row and col pixels to pad, defaults to 1
         :type  padding:      Union[int, Tuple[int, int]], optional
+        :param groups:       channel groups in input, defaults to 1
+        :type  groups:       int, optional
         """
         super(ConvBatchRelu, self).__init__()
         self.add_module('conv', nn.Conv2d(
@@ -42,52 +44,46 @@ class ConvBatchRelu(Sequential):
         self.add_module('relu', nn.ReLU())
 
 
-class ResidualBottleneck(Module):
-    """ Residual block: 1 x 1, 3 x 3, 1 x 1, full pre-activation.
-        See https://arxiv.org/pdf/1603.05027.pdf Fig 4e.
+class BatchReluConv(Sequential):
+    """ Convnet building block: batchnorm, relu, conv (pre-activation).
+        See https://arxiv.org/pdf/1603.05027.pdf.
     """
     def __init__(self,
                  in_channels: int,
-                 bottle_channels: int) -> None:
+                 out_channels: int,
+                 kernel_size: Union[int, Tuple[int, int]] = 3,
+                 stride: Union[int, Tuple[int, int]] = 1,
+                 padding: Union[int, Tuple[int, int]] = 1,
+                 groups: int = 1,
+                 bias: bool = False) -> None:
         """ Constructor.
 
-        :param in_channels:     number of input channels
-        :type  in_channels:     int
-        :param bottle_channels: number of bottleneck channels (< in_channels)
-        :type  bottle_channels: int
+        :param in_channels:  number of input channels
+        :type  in_channels:  int
+        :param out_channels: number of output channels
+        :type  out_channels: int
+        :param kernel_size:  kernel height and width, defaults to 3
+        :type  kernel_size:  Union[int, Tuple[int, int]], optional
+        :param stride:       stride height and width, defaults to 1
+        :type  stride:       Union[int, Tuple[int, int]], optional
+        :param padding:      row and col pixels to pad, defaults to 1
+        :type  padding:      Union[int, Tuple[int, int]], optional
+        :param groups:       channel groups in input, defaults to 1
+        :type  groups:       int, optional
+        :param bias:         whether conv layer uses bias, defaults to False
+        :type  bias:         bool, optional
         """
-        super(ResidualBottleneck, self).__init__()
-        self.residual = Sequential(
-            nn.BatchNorm2d(in_channels),       # preactivation
-            nn.ReLU(),                         # preactivation
-            ConvBatchRelu(
-                in_channels=in_channels,
-                out_channels=bottle_channels,
-                kernel_size=1,
-                stride=1,
-                padding=0),                    # 1 x 1
-            ConvBatchRelu(
-                in_channels=bottle_channels,
-                out_channels=bottle_channels,
-                kernel_size=3,
-                stride=1,
-                padding=1),                    # 3 x 3
-            nn.Conv2d(
-                in_channels=bottle_channels,
-                out_channels=in_channels,
-                kernel_size=1,
-                stride=1,
-                padding=0))                    # 1 x 1
-
-    def forward(self, inp: Tensor) -> Tensor:
-        """ Compute residual and add to main path.
-
-        :param inp: feature map (N, D, H, W)
-        :type  inp: Tensor
-        :return:    residual-adjusted feature map
-        :rtype:     Tensor
-        """
-        return inp + self.residual(inp)
+        super(BatchReluConv, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(in_channels))
+        self.add_module('relu', nn.ReLU())
+        self.add_module('conv', nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            groups=groups,
+            bias=bias))
 
 
 class GapSoftmax(Module):
--- wz_torch/conv/residual.py
+++ wz_torch/conv/residual.py
@@ -2,10 +2,66 @@
 
 ResNeXt https://arxiv.org/pdf/1611.05431.pdf (Apr 2017).
 """
+from typing import Tuple, Union
 from torch import Tensor
 import torch.nn as nn
 from torch.nn import Module
-from . import ConvBatchRelu
+from . import ConvBatchRelu, BatchReluConv
+
+
+class ResidualBottleneck(Module):
+    """ Residual block: 1 x 1, 3 x 3, 1 x 1, full pre-activation.
+        See https://arxiv.org/pdf/1603.05027.pdf Fig 4e.
+    """
+    def __init__(self,
+                 in_channels: int,
+                 bottle_channels: int,
+                 kernel_size: Union[int, Tuple[int, int]] = 3,
+                 stride: Union[int, Tuple[int, int]] = 1,
+                 padding: Union[int, Tuple[int, int]] = 1) -> None:
+        """ Constructor.
+
+        :param in_channels:     number of input channels
+        :type  in_channels:     int
+        :param bottle_channels: number of bottleneck channels (< in_channels)
+        :type  bottle_channels: int
+        :param kernel_size:     kernel height and width, defaults to 3
+        :type  kernel_size:     Union[int, Tuple[int, int]], optional
+        :param stride:          stride height and width, defaults to 1
+        :type  stride:          Union[int, Tuple[int, int]], optional
+        :param padding:         row and col pixels to pad, defaults to 1
+        :type  padding:         Union[int, Tuple[int, int]], optional
+        """
+        super(ResidualBottleneck, self).__init__()
+        self.residual = nn.Sequential(
+            BatchReluConv(
+                in_channels=in_channels,
+                out_channels=bottle_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0),                    # 1 x 1
+            BatchReluConv(
+                in_channels=bottle_channels,
+                out_channels=bottle_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding),              # 3 x 3
+            BatchReluConv(
+                in_channels=bottle_channels,
+                out_channels=in_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0))                    # 1 x 1
+
+    def forward(self, inp: Tensor) -> Tensor:
+        """ Compute residual and add to main path.
+
+        :param inp: feature map (N, D, H, W)
+        :type  inp: Tensor
+        :return:    residual-adjusted feature map
+        :rtype:     Tensor
+        """
+        return inp + self.residual(inp)
 
 
 class ResNeXt(Module):
